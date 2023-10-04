66b9164ff2ac7389b1951f7bd70b238134b09b5f
Mario <mariosantos88477364484@gmail.com>
2023-10-04 11:11:21 -0300

    header web

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,3 +1,13 @@
+import avatar from "../assets/avatar.png";
+
 export function Header() {
-    return <div>Header</div>;
+    return (
+        <header className="flex items-center justify-between p-4">
+            <h1 className="font-bold">Bom dia, Nome do usuário! 👋</h1>
+            <button className="flex items-center justify-center gap-4">
+                <span>Nome do usuário</span>
+                <img src={avatar} className="h-[30px] w-[30px]" />
+            </button>
+        </header>
+    );
 }
