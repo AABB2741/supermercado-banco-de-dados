4437de38e2eda5c8cdf8e7a9f4f912fbc1f48f79
Mario <mariosantos88477364484@gmail.com>
2023-10-05 22:58:27 -0300

    signup controller server

--- server/src/controllers/auth/SignUpController.ts
+++ server/src/controllers/auth/SignUpController.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import z from "zod";
 
 import { SignUpUseCase } from "../../useCases/auth/SignUpUseCase";
 
-export async function SignUpController(req: Request, res: Response) {}
+export async function SignUpController(req: Request, res: Response) {
+	const bodySchema = z.object({
+		name: z.string().min(3).max(100),
+		email: z.string().email(),
+		password: z.string().min(4).max(50),
+	});
+
+	const credentials = bodySchema.parse(req.body);
+	const user = await SignUpUseCase(credentials);
+	const token = jwt.sign(
+		{
+			id: user.id,
+		},
+		process.env.JWT_SECRET as string
+	);
+
+	res.status(201).json({ ...user, token });
+}
--- server/src/useCases/auth/SignUpUseCase.ts
+++ server/src/useCases/auth/SignUpUseCase.ts
@@ -1,1 +1,26 @@
-export async function SignUpUseCase() {}
+import { SHA256 } from "crypto-js";
+
+import { prisma } from "../../prisma";
+
+interface SignUpProps {
+	name: string;
+	email: string;
+	password: string;
+}
+
+export async function SignUpUseCase({ name, email, password }: SignUpProps) {
+	const user = await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: SHA256(password).toString(),
+		},
+		select: {
+			id: true,
+			name: true,
+			email: true,
+		},
+	});
+
+	return user;
+}
