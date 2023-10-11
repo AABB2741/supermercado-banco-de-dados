31db42023c3cf6e489311270429dcc9547c6661e
Mario <mariosantos88477364484@gmail.com>
2023-10-11 16:13:36 -0300

    botões banner

--- web/src/pages/List/Banner.tsx
+++ web/src/pages/List/Banner.tsx
@@ -1,4 +1,6 @@
 import { Check, Clock } from "lucide-react";
+import { Link } from "react-router-dom";
+import { ArrowLeft, Settings } from "lucide-react";
 
 import { useList } from "../../contexts/ListProvider";
 
@@ -16,10 +18,14 @@ export function Banner() {
             style={{ backgroundImage: `url(${listBanner})` }}
         >
             {/* Top */}
-            <div></div>
+            <div className="flex p-10">
+                <Link to="/dashboard/" className="rounded-lg bg-black/50 p-3">
+                    <ArrowLeft size={20} />
+                </Link>
+            </div>
 
             {/* Bottom */}
-            <div className="bg-gradient-to-t from-black to-transparent p-10">
+            <div className="flex justify-between bg-gradient-to-t from-black to-transparent p-10">
                 {/* Left */}
                 <div>
                     <div className="mb-2 inline-flex items-center gap-2 rounded-full bg-black/50 px-6 py-2 text-yellow-400">
@@ -56,7 +62,14 @@ export function Banner() {
                 </div>
 
                 {/* Right */}
-                <div></div>
+                <div className="flex items-center gap-8 self-end">
+                    <button>
+                        <Settings size={32} />
+                    </button>
+                    <button className="aspect-square rounded-full bg-sky-500 p-4 text-white">
+                        <Check />
+                    </button>
+                </div>
             </div>
         </div>
     );
