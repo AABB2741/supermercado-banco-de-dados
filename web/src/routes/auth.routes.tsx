2b373fa61c85c811e74183d177f454e556d14e24
Mario <mariosantos88477364484@gmail.com>
2023-10-12 14:30:48 -0300

    melhorando responsividade auth

--- web/src/routes/auth.routes.tsx
+++ web/src/routes/auth.routes.tsx
@@ -4,12 +4,12 @@ import authBackground from "../assets/auth-background.jpg";
 
 export function AuthRoutes() {
     return (
-        <div className="flex max-h-screen min-h-screen flex-1 items-center justify-between overflow-y-hidden">
+        <div className="flex max-h-screen min-h-screen flex-1 flex-col items-center justify-between overflow-y-auto lg:flex-row">
             <img
                 src={authBackground}
-                className="min-h-screen w-1/2 object-cover"
+                className="h-[50vh] w-full object-cover"
             />
-            <div className="mx-auto max-h-screen max-w-[600px] flex-1 overflow-y-auto p-8">
+            <div className="mx-auto max-w-[600px] flex-1 p-8">
                 <Outlet />
             </div>
         </div>
