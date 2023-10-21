c597c100a933644801982100d35352b1c79a8c12
Mario <mariosantos88477364484@gmail.com>
2023-10-21 12:44:04 -0300

    colocando novos controllers na rota

--- server/src/routes/pantry.routes.ts
+++ server/src/routes/pantry.routes.ts
@@ -4,6 +4,8 @@ import { authHandler } from "../middlewares/authHandler";
 
 import { getPantryItemsController } from "../controllers/pantry/getPantryItemsController";
 import { addPantryItemController } from "../controllers/pantry/addPantryItemController";
+import { editPantryItemController } from "../controllers/pantry/editPantryItemController";
+import { removePantryItemController } from "../controllers/pantry/removePantryItemController";
 
 const pantryRoutes = Router();
 
@@ -11,5 +13,7 @@ pantryRoutes.use(authHandler);
 
 pantryRoutes.get("/items", getPantryItemsController);
 pantryRoutes.post("/add", addPantryItemController);
+pantryRoutes.put("/edit/:id", editPantryItemController);
+pantryRoutes.delete("/remove/:id", removePantryItemController);
 
 export default pantryRoutes;
