cb72f565329714accee6a3358a4a075ca39c4e08
Mario <mariosantos88477364484@gmail.com>
2023-10-21 10:21:27 -0300

    pantryprovider

---
+++ web/src/contexts/PantryProvider.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { PantryItemProps } from "../@types/pantry-item-props";
+import axios from "axios";
+import { getPantryItems } from "../services/pantry/getPantryItems";
+
+interface PantryContextValue {
+    items: PantryItemProps[];
+    setItems: React.Dispatch<
+        React.SetStateAction<PantryItemProps[] | undefined>
+    >;
+}
+
+interface PantryItemRootProps {
+    children?: React.ReactNode;
+}
+
+const PantryContext = createContext({} as PantryContextValue);
+
+export function PantryItemRoot({ children }: PantryItemRootProps) {
+    const [items, setItems] = useState<PantryItemProps[]>();
+
+    useEffect(() => {
+        const cancelToken = axios.CancelToken.source();
+
+        getPantryItems(cancelToken.token)
+            .then((res) => {
+                setItems(res);
+            })
+            .catch((err) => {
+                if (!axios.isCancel(err)) {
+                    // TODO: Handle error
+                }
+            });
+
+        return cancelToken.cancel;
+    }, []);
+
+    if (!items) return null;
+
+    return <div className="p-8">{children}</div>;
+}
+
+export const usePantry = () => useContext(PantryContext);
