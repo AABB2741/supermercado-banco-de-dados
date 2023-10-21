b950625eb64ee563f54af9027f2591208e7ced62
Mario <mariosantos88477364484@gmail.com>
2023-10-21 12:37:05 -0300

    pantrylistitem options

--- web/src/pages/Dashboard/Pantry/PantryList/PantryListItem.tsx
+++ web/src/pages/Dashboard/Pantry/PantryList/PantryListItem.tsx
@@ -1,3 +1,4 @@
+import { PenLine, X } from "lucide-react";
 import { PantryItem } from "../../../../services/pantry/getPantryItems";
 
 export function PantryListItem({ id, isOffline, amount, product }: PantryItem) {
@@ -6,11 +7,15 @@ export function PantryListItem({ id, isOffline, amount, product }: PantryItem) {
             <span className="flex h-10 w-10 items-center justify-center rounded-lg bg-sky-500">
                 {product.name[0]}
             </span>
-            <div>
+            <div className="flex-1">
                 <p>{product.name}</p>
                 <p className="flex items-center gap-2 text-sm text-gray-600 dark:text-zinc-400">
                     <span>x{amount}</span>
                 </p>
+            </div>
+            <div className="flex items-center gap-2">
+                <PenLine size={16} />
+                <X size={16} className="text-red-600" />
             </div>
         </li>
     );
