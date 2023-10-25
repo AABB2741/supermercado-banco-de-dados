2623c16f55b9b86b091ffb40f11023b645537c81
Mario <mariosantos88477364484@gmail.com>
2023-10-24 22:10:19 -0300

    create product backend

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import authRoutes from "./auth.routes";
 import listRoutes from "./list.routes";
 import pantryRoutes from "./pantry.routes";
-// import productRoutes from "./product.routes";
+import productRoutes from "./product.routes";
 
 const routes = Router();
 
@@ -11,6 +11,6 @@ const routes = Router();
 routes.use("/auth", authRoutes);
 routes.use("/lists", listRoutes);
 routes.use("/pantry", pantryRoutes);
-// routes.use("/products", productRoutes);
+routes.use("/products", productRoutes);
 
 export default routes;
