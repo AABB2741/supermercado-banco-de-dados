c6b3fbdc3bb28bf3dc10baeea4f4735a7a65e52c
Mario <mariosantos88477364484@gmail.com>
2023-11-09 23:21:23 -0300

    Adicionando novos produtos padrão

--- server/src/seed.ts
+++ server/src/seed.ts
@@ -6,19 +6,23 @@ import { defaultProducts } from "./data/defaultProducts";
 
 /** Esta função deve ser executada toda vez que o banco de dados for zerado. Ele contém algumas informações padrões necessárias, como o usuário RPB Shopping e produtos para listas de compras */
 export async function setup() {
+	console.log(`Iniciando dados padrão para o banco de dados...`);
 	// Criação de usuário padrão
 	const defaultUser = {
 		name: "RPB Shopping",
 		email: "default@rpbshopping.com",
 		password: SHA256("rpb-shopping-default-user-password").toString(),
 	};
+	console.log("Criando o usuário...");
 
 	const user = await prisma.user.create({
 		data: defaultUser,
 	});
+	console.log(`Criado o usuário ${user.name} com id ${user.id}`);
 
+	console.log("Adicionando produtos padrão...");
 	// Inserção de produtos padrão
-	await prisma.$transaction(
+	const res = await prisma.$transaction(
 		defaultProducts.map((p) =>
 			prisma.product.create({
 				data: {
@@ -29,5 +33,10 @@ export async function setup() {
 			})
 		)
 	);
+	console.log(
+		`${res.length} produtos foram adicionados ao banco de dados para o usuário padrão`
+	);
+
+	console.log("Finalizado a função de dados padrão.");
 }
 setup();
