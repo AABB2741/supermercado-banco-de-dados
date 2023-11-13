4f5e6a876b18eadc37c0271f5b2a6fdebadf85f9
Mario <mariosantos88477364484@gmail.com>
2023-11-13 08:37:47 -0300

    Iniciando lógica produtos recomendados

--- server/src/useCases/product/getSuggestedProductsUseCase.ts
+++ server/src/useCases/product/getSuggestedProductsUseCase.ts
@@ -6,5 +6,36 @@ export async function getSuggestedProductsUseCase(
 ) {
 	// Pega todos os produtos que o usuário já comprou/usou
 	// Retorna o promise para o transaction em getProductsUseCase.
+
+	const history = await prisma.pantryHistory.findMany({
+		where: {
+			userId,
+		},
+		include: {
+			product: true,
+		},
+		orderBy: {
+			id: "asc",
+		},
+	});
+
+	// Pega todas as vezes que o usuario gastou um produto
+	// TODO: Arrumar, está pegando de TODOS os produtos
+	const consumption = [];
+
+	for (let i = 0; i < history.length; i++) {
+		if (i === 0) continue;
+
+		const h = history[i];
+
+		const difference = h.amount - history[i - 1].amount;
+
+		if (difference > 0) {
+			consumption.push(h);
+		}
+	}
+
+	console.log(consumption);
+
 	return [];
 }
