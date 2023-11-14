af7f69821e08f7eab2058e1cadc04d43c84c7c15
Mario <mariosantos88477364484@gmail.com>
2023-11-13 21:54:15 -0300

    arrumando responsividade

--- web/src/components/NavBar/NavBarTab.tsx
+++ web/src/components/NavBar/NavBarTab.tsx
@@ -14,15 +14,15 @@ export function NavBarTab({ label, icon: Icon, ...rest }: NavBarTabProps) {
     return (
         <Link
             {...rest}
-            className="relative flex items-center justify-center gap-4 px-4 py-2 after:absolute after:top-full after:mt-1 after:rounded-full data-[selected=true]:pointer-events-none data-[selected=true]:font-bold data-[selected=true]:after:h-[5px] data-[selected=true]:after:w-1/2 data-[selected=true]:after:bg-sky-500"
+            className="relative my-3 flex items-center justify-center gap-3 py-1 after:absolute after:top-full after:mt-1 after:rounded-full data-[selected=true]:pointer-events-none data-[selected=true]:font-bold data-[selected=true]:after:h-[5px] data-[selected=true]:after:w-1/2 data-[selected=true]:after:bg-sky-500 md:gap-4 md:px-4"
             data-selected={path === rest.to}
         >
             <Icon
-                className="data-[selected=true]:fill-current"
+                className="hidden data-[selected=true]:fill-current lg:block"
                 data-selected={path === rest.to}
                 fill={path === rest.to ? "fill" : "transparent"}
             />
-            <p>{label}</p>
+            <span className="whitespace-nowrap">{label}</span>
         </Link>
     );
 }
--- web/src/pages/Dashboard/Lists/index.tsx
+++ web/src/pages/Dashboard/Lists/index.tsx
@@ -17,7 +17,7 @@ export function Lists() {
             <section className="flex justify-center">
                 <CreateList />
             </section>
-            <section className="grid grid-cols-1 gap-6 px-8 py-8 md:grid-cols-2 xl:grid-cols-3">
+            <section className="grid grid-cols-1 gap-6 px-4 py-8 md:grid-cols-2 md:px-8 xl:grid-cols-3">
                 {lists?.map((list) => <ListButton key={list.id} {...list} />)}
             </section>
         </div>
