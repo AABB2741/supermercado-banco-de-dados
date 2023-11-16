06e6e1f70b8f5980e456a35f9201d2eda2104cf0
Mario <mariosantos88477364484@gmail.com>
2023-11-16 16:46:13 -0300

    Update AddItemPreview.tsx

--- web/src/pages/List/AddItem/AddItemPreview.tsx
+++ web/src/pages/List/AddItem/AddItemPreview.tsx
@@ -28,8 +28,6 @@ export function AddItemPreview() {
         activeElement === priceRef.current ||
         editLoading;
 
-    console.log("Disabled:", disabled);
-
     useEffect(() => {
         setDisabled(disabled);
     }, [disabled, setDisabled]);
