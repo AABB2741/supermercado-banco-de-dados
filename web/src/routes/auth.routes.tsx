78a239341cb70975941c8ad897eb1cd91ff6ae95
Mario <mariosantos88477364484@gmail.com>
2023-11-16 09:52:37 -0300

    arrumando banco de dados

--- web/src/routes/auth.routes.tsx
+++ web/src/routes/auth.routes.tsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 import { Carousel } from "../components/Carousel";
 
 import authBackground from "../assets/auth-background.jpg";
-import { Slider } from "@radix-ui/react-slider";
 
 export function AuthRoutes() {
     const [slide, setSlide] = useState(1);
